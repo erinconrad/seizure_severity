3e3b902e188ce688f176a54feec6d36d823e2253
Erin <erincconrad@gmail.com>
2025-11-21 20:08:22 -0500

    multithresh

--- spike_counts_varythresh.m
+++ spike_counts_varythresh.m
@@ -106,7 +106,7 @@ else
 end
 
 %% ===== Process each CSV =====
-for k = 1:20%numel(files)
+for k = 1:numel(files)
     fpath  = fullfile(files(k).folder, files(k).name);
     fname  = files(k).name;
 
